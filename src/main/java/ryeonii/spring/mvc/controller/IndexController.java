42b78e0312079fbf4de4eb2b88d0ba5b3b787877
ryeonii <gpfus9619@naver.com>
2020-05-26 16:03:42 +0900

    pom.xml indexController 수정

--- src/main/java/ryeonii/spring/mvc/controller/IndexController.java
+++ src/main/java/ryeonii/spring/mvc/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class indexController {
+public class IndexController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView index(){
